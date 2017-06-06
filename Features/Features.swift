583597b6b4344337ec1b64b16a6dd4d7142f9403
Gavan Chan <gavan.chan@outware.com.au>
2017-06-06 23:39:21 +1000

    Provide integration / 'unit' tests

--- Features/Features.swift
+++ Features/Features.swift
@@ -7,7 +7,35 @@ final class Features: XCTestCase {
     XCUIApplication().launch()
   }
 
-  func testExample() { }
+  func testCyclingOfData() {
+    let range: CountableClosedRange = 1...7
+    for currentValue in range {
+      XCTAssertTrue(funky.staticTexts["ID: \(currentValue)"].exists)
+      nextButton.tap()
+    }
+  }
+
+  func testPeopleWhoAreFreakingAncient() {
+    while !funky.staticTexts["Name: Squire Gavan Chan"].exists {
+      nextButton.tap()
+    }
+
+    XCTAssertTrue(funky.staticTexts["Gender: Male. Age: Ancient."].exists)
+  }
+
+  func testAndyIsANoob() {
+    while !funky.staticTexts["ID: 7"].exists {
+      nextButton.tap()
+    }
+
+    XCTAssertTrue(funky.staticTexts["Name: n0ob Andy Chen"].exists)
+  }
+
+  private let funky: XCUIApplication = XCUIApplication()
+
+  private var nextButton: XCUIElement {
+    return funky.buttons["Next"]
+  }
 
 }
 
